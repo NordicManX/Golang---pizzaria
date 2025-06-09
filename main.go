301f4433589963dc9a7c95b556d69f290df9ec49
Nelson Carvalho <nelsonfilth@hotmail.com>
2025-06-09 16:08:21 -0300

    created new models pizzaria

--- main.go
+++ main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
+	"pizzaria/models"
+
 	"github.com/gin-gonic/gin"
 )
 
-type Pizza struct {
-	ID    int
-	Nome  string
-	preco float64
-}
-
 func main() {
+
 	r := gin.Default()
-	r.GET("/pizzas", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"pizzas": "Toscana, Calabresa sem cebola",
-		})
-	})
-
-	// var pizzas = []Pizza{
-
-	// 	{ID: 1, Nome: "Toscana", preco: 49.50},
-	// 	{ID: 2, Nome: "Marguerita", preco: 79.50},
-	// 	{ID: 3, Nome: "Atum com queijo", preco: 69.50},
-	// }
-	// fmt.Println(pizzas)
+	r.GET("/pizzas", getPizzas)
 	r.Run()
+}
+func getPizzas(c *gin.Context) {
+	var pizzas = []models.Pizza{
+
+		{ID: 1, Nome: "Toscana", Preco: 49.50},
+		{ID: 2, Nome: "Marguerita", Preco: 79.50},
+		{ID: 3, Nome: "Atum com queijo", Preco: 69.50},
+	}
+
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
 }
